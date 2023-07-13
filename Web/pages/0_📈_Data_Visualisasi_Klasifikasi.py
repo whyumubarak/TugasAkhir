7a83a5b9bf22225faa4e60f4bfa322c69de045c1
Wahyu Mubarak S <117147246+whyumubarak@users.noreply.github.com>
2023-07-13 18:25:46 +0700

    Update 0_📈_Data_Visualisasi_Klasifikasi.py

--- Web/pages/0_📈_Data_Visualisasi_Klasifikasi.py
+++ Web/pages/0_📈_Data_Visualisasi_Klasifikasi.py
@@ -458,7 +458,7 @@ if visualisasi == "Metode Kernel Extreme Learning Machine":
         return y
 
     def determine_air_quality(PM10, SO2, CO, O3, NO2):
-        model = joblib.load('model_leaky.sav')
+        model = joblib.load('Web/model_leaky.sav')
         W_test, b_test = model['W'], model['b']
 
         X_test = np.array([[PM10, SO2, CO, O3, NO2]])

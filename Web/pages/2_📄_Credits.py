76af6cd4f04a76664e0123c94eadf6599e70afdf
Wahyu Mubarak S <117147246+whyumubarak@users.noreply.github.com>
2023-07-29 19:41:40 +0700

    Update 2_📄_Credits.py

--- Web/pages/2_📄_Credits.py
+++ Web/pages/2_📄_Credits.py
@@ -27,21 +27,21 @@ st.write("dibuat oleh:")
 
 col1, col2, col3 = st.columns(3)
 with col1:
-    image = Image.open('Web/images/Wahyu.jpeg')
+    image = Image.open('Web/images/x.jpg')
     scale_factor = 0.5 
     new_width = int(image.width * scale_factor)
     new_height = int(image.height * scale_factor)
     resized_image = image.resize((new_width, new_height))
     st.image(resized_image, caption='WAHYU MUBARAK SUKIMAN')
 with col2:
-    image = Image.open('Web/images/agung.jpg')
+    image = Image.open('Web/images/y.jpg')
     scale_factor = 0.5 
     new_width = int(image.width * scale_factor)
     new_height = int(image.height * scale_factor)
     resized_image = image.resize((new_width, new_height))
     st.image(resized_image, caption='AGUNG SULAKSONO RAMDHANI')
 with col3:
-    image = Image.open('Web/images/obie.jpg')
+    image = Image.open('Web/images/z.jpg')
     scale_factor = 0.5 
     new_width = int(image.width * scale_factor)
     new_height = int(image.height * scale_factor)
